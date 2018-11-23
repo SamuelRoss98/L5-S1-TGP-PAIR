f1633aaec3d610af0465dd42f5f8a1d1b55b5864
Sam Johnson <sam.ross.johnson@gmail.com>
2018-11-23 11:24:51 +0000

    Created CombatantAIController and CombatantPlayerController

---
+++ TGPPair/Source/TGPPair/Public/CombatantPlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CombatantPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TGPPAIR_API ACombatantPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
