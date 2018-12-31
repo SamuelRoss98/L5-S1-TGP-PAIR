eeee72de2a23565585b8a9faf3ab8ff40cf50ec8
Sam Johnson <sam.ross.johnson@gmail.com>
2018-12-31 09:56:27 +0000

    Reimplemented spawning code from previous version

--- TGPPair/Source/TGPPair/Private/CombatGameModeBase.cpp
+++ TGPPair/Source/TGPPair/Private/CombatGameModeBase.cpp
@@ -2,6 +2,104 @@
 
 #include "CombatGameModeBase.h"
 
+#include "Engine/World.h"
+#include "CombatantPawn.h"
+#include "CombatDecisionInterface.h"
+#include "CombatAIController.h"
 
+// Called when the game starts or when spawned
+void ACombatGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
 
+	SpawnCombatants();
+}
 
+// Called every frame
+void ACombatGameModeBase::Tick(float DeltaTime)
+{
+}
+
+// Resets the combatants ready for a new round.
+void ACombatGameModeBase::ResetForNewRound()
+{
+	for (int i = 0; i < EnemyCombatants.Num(); ++i)
+	{
+		if (EnemyCombatants[i] != nullptr)
+		{
+			EnemyCombatants[i]->SetTurnTaken(false);
+			EnemyCombatants[i]->MakeCombatDescision();
+		}
+	}
+
+	if (PlayerCombatant != nullptr)
+	{
+		PlayerCombatant->SetTurnTaken(false);
+		PlayerCombatant->MakeCombatDescision();
+	}
+}
+
+// Spawns combatants.
+void ACombatGameModeBase::SpawnCombatants()
+{
+	// Check there are enough spawn locations given to spawn all combatants.
+	if (EnemyCount + 1 > SpawnLocations.Num())
+	{
+		UE_LOG(LogTemp, Error, TEXT("There are no enough spawn points to spawn the combatants."))
+			return;
+	}
+
+	FVector SpawnLoc = FVector::ZeroVector;
+	FRotator SpawnRot = FRotator::ZeroRotator;
+	FActorSpawnParameters SpawnParams;
+
+	// Spawn player.
+	SpawnLoc = SpawnLocations[0];
+	PlayerCombatant = Cast<ACombatantPawn>(GetWorld()->SpawnActor(CombatantClass, &SpawnLoc, &SpawnRot, SpawnParams));
+	if (PlayerCombatant == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to spawn the player combatant."))
+			return;
+	}
+	GetWorld()->GetFirstPlayerController()->SetPawn(PlayerCombatant);
+	if (!PlayerCombatant->Initialize(FNamedStatPack(), true, Cast<ICombatDecisionInterface>(GetWorld()->GetFirstPlayerController())))
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to initialize the player combatant."))
+			return;
+	}
+
+	// Spawn enemies.
+	for (int i = 0; i < EnemyCount; ++i)
+	{
+		// Create the enemy combatant.
+		ACombatantPawn* NewEnemy = nullptr;
+		SpawnLoc = SpawnLocations[i + 1];
+		NewEnemy = Cast<ACombatantPawn>(GetWorld()->SpawnActor(CombatantClass, &SpawnLoc, &SpawnRot, SpawnParams));
+		if (NewEnemy == nullptr)
+		{
+			UE_LOG(LogTemp, Error, TEXT("Failed to spawn an enemy combatant."))
+				return;
+		}
+
+		// Create the AI controller.
+		ACombatAIController* Controller = nullptr;
+		Controller = Cast<ACombatAIController>(GetWorld()->SpawnActor(AIClass, &SpawnLoc, &SpawnRot, SpawnParams));
+		if (Controller == nullptr)
+		{
+			UE_LOG(LogTemp, Error, TEXT("Failed to spawn an enemy combatant AI controller."))
+				return;
+		}
+		Controller->SetPawn(NewEnemy);
+
+		// Init the enemy combatant.
+		if (!NewEnemy->Initialize(FNamedStatPack(), false, Cast<ICombatDecisionInterface>(Controller)))
+		{
+			UE_LOG(LogTemp, Error, TEXT("Failed to initialize an enemy combatant."))
+				return;
+		}
+		EnemyCombatants.Add(NewEnemy);
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Combatants spawned successfully."))
+		StartNewRound();
+}
--- TGPPair/Source/TGPPair/Public/CombatGameModeBase.h
+++ TGPPair/Source/TGPPair/Public/CombatGameModeBase.h
@@ -6,15 +6,56 @@
 #include "GameFramework/GameModeBase.h"
 #include "CombatGameModeBase.generated.h"
 
+class ACombatantPawn;
+class ACombatAIController;
+
 /**
- * 
- */
+*
+*/
 UCLASS()
 class TGPPAIR_API ACombatGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
-	
-	
-	
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Spawns combatants.
+	void SpawnCombatants();
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Event called at the start of each new round.
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartNewRound();
+
+	// Resets the combatants ready for a new round.
+	UFUNCTION(BlueprintCallable)
+	void ResetForNewRound();
+
+public:
+	// Number of enemies to spawn.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int EnemyCount = 1;
+
+	// Spawn positions for combatants, index 0 is for the player, the next three will be used for enemy 1, 2, then 3 if needed.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FVector> SpawnLocations;
+
+	// Class type to spawn combatants from.
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<ACombatantPawn> CombatantClass;
+
+	// Class type for enemy AI controller.
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<ACombatAIController> AIClass;
+
+	// Combatant controlled by the player.
+	ACombatantPawn* PlayerCombatant = nullptr;
+
+	// Contains all enemy combatants.
+	TArray<ACombatantPawn*> EnemyCombatants = { nullptr };
 };
